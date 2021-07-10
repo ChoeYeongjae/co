495fc9116a60230d39e46db42c085de752dc67ee
CYJ <CYJ@DESKTOP-IVPQUM2>
2021-07-10 22:37:59 +0900

    d

---
+++ coinLatss/src/main/java/com/coinLatss/CoinLatssApplication.java
@@ -0,0 +1,13 @@
+package com.coinLatss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinLatssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinLatssApplication.class, args);
+	}
+
+}
---
+++ coinLatss/src/test/java/com/coinLatss/CoinLatssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coinLatss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinLatssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
